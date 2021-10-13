36ee8e006b05c95e7189cf84881b4dd0e99ad49d
Levy Srugo <41137165+lsrugo770@users.noreply.github.com>
2021-10-13 04:51:04 +0000

    add login.html to build

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+/*global require,module,__dirname*/
+const { resolve } = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'login.html')
+      }
+    }
+  }
+})
